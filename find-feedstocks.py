820e50d28ed47bc41f47dadefb01ac02cb2722dc
christopher <cjwright4242@gmail.com>
2018-02-09 21:15:35 -0500

    WIP: run the thing

--- find-feedstocks.py
+++ find-feedstocks.py
@@ -4,8 +4,8 @@ gh = github3.GitHub()
 org = gh.organization('conda-forge')
 with open('names.txt', 'w') as f:
     for repo in org.iter_repos():
-        name = repo.full_name.strip('conda-forge/')
+        name = repo.full_name.split('conda-forge/')[-1]
         if 'feedstock' in name and 'feedstocks' not in name:
-            f.write(name.strip('-feedstock'))
+            f.write(name.split('-feedstock')[0])
             f.write('\n')
 
